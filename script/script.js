f4412f004234d5f4a897a3b2308183d5b65933ca
kesavan <kesavanplk7@gmail.com>
2023-02-27 14:18:26 +0530

    click function added

--- script/script.js
+++ script/script.js
@@ -3,3 +3,14 @@
 const num = document.getElementById("guess-num");
 const btn = document.getElementById("btn-guess");
 const output = document.getElementById("output-val");
+
+// getting random number
+
+const randomNum = Math.trunc(Math.random() * 50 + 1);
+const guessNum = num.value;
+console.log(num.value);
+btn.addEventListener("click", function (range) {
+  if (randomNum === guessNum) {
+    console.log();
+  }
+});
